248a5cf6dbeadaffbbe7e8cd802b3a56d4bb8ef5
Aditya <saditya2094@gmail.com>
2025-06-05 01:06:18 +0530

    json output parser

---
+++ OutputParser/json_output_parser.py
@@ -0,0 +1,22 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatGoogleGenerativeAI(model='gemini-2.0-flash-001')
+
+parser = JsonOutputParser()
+
+template = PromptTemplate(
+    template='give me name, age and city of a fictional person \n {format_instruction}',
+    input_variables=[],
+    partial_variables={'format_instruction': parser.get_format_instructions()}
+)
+
+prompt = template.format()
+
+chain = template | model | parser
+result = chain.invoke({}) # need to pass an empty object if the input variable in prompt template in null
+print(result)
