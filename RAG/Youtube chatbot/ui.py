6a52f06720e0bd983985fe31b19f1b8820df7bef
Aditya <adityas.singh.tech@gmail.com>
2025-07-05 10:32:24 +0530

    feat: create two column structure to show transcript to the user

--- RAG/Youtube chatbot/ui.py
+++ RAG/Youtube chatbot/ui.py
@@ -1,14 +1,18 @@
 import streamlit as st
 
 Language_options = [
-    "ar", "bn", "bg", "zh", "hr", "cs", "da", "nl", "en", "et",
-  "fi", "fr", "de", "el", "iw", "hi", "hu", "id", "it", "ja",
+    "en", "hi", "bg", "zh", "hr", "cs", "da", "nl", "ar", "et",
+  "fi", "fr", "de", "el", "iw", "bn", "hu", "id", "it", "ja",
   "ko", "lv", "lt", "no", "pl", "pt", "ro", "ru", "sr", "sk",
   "sl", "es", "sw", "sv", "th", "tr", "uk", "vi"
 ]
 
+
 def show_ui():
+  st.header('Chat with Youtube videos')
+  col1,col2 = st.columns(2)
+  with col1:
     yt_video = st.text_input("Youtube video link:",placeholder='Enter youtube video Link')
     question = st.text_input("Your Question:",placeholder='Ask anything about the video')
     language = st.selectbox("Transcript Language:",Language_options)
-    return yt_video,question,language
+  return yt_video,question,language,col1,col2
